1ee7335c3ae9f142e9f5dbec909d3c48f77f28e7
begum-yuzgulen <begum.yuzgulen@gmail.com>
2020-04-22 18:33:50 +0300

    Update spot.js

--- routes/spot.js
+++ routes/spot.js
@@ -8,17 +8,16 @@ const spotRouter = express.Router();
 spotRouter.use(bodyParser.json());
 
 var connection = mysql.createConnection(config.credentials);
+connection.connect(function(err) {
+    if (err) {
+      console.log('error: ' + err.message);
+    }
+    console.log('Connected to the MySQL server.');
+});
 
 spotRouter.route('/').get( (req,res,next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    connection.connect(function(err) {
-        if (err) {
-          console.log('error: ' + err.message);
-        }
-      
-        console.log('Connected to the MySQL server.');
-      });
     
     connection.query('SELECT * FROM Spot', function (err, rows, fields) {
         try{
@@ -27,19 +26,10 @@ spotRouter.route('/').get( (req,res,next) => {
             console.log(e);
         }
     });
-    connection.end();
 });
 spotRouter.route('/:sectionId').get( (req,res,next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-
-    connection.connect(function(err) {
-        if (err) {
-          console.log('error: ' + err.message);
-        }
-      
-        console.log('Connected to the MySQL server.');
-      });
     
     connection.query(`SELECT * FROM Spot WHERE ID LIKE '${req.params.sectionId}%'`, function (err, rows, fields) {
         try{
@@ -48,8 +38,6 @@ spotRouter.route('/:sectionId').get( (req,res,next) => {
             console.log(e);
         }
     });
-    connection.end();
 });
-
 
 module.exports = spotRouter;
