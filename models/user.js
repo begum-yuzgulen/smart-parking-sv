db16f2d9753a15ba5968bc0698ca91bc2442080d
begum-yuzgulen <begum.yuzgulen@gmail.com>
2021-01-24 00:07:18 +0200

    added username to schema

--- models/user.js
+++ models/user.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 const User = new Schema({
+    username: {
+        type: String,
+        default: ''
+    },
     firstname: {
         type: String,
         default: ''
